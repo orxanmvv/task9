3207360587febb33f80e0369cd7af547949f41ad
orxanmvv <m.orxan.m123@gmail.com>
2024-04-16 10:05:19 +0400

    9-cu task

---
+++ ConsoleApp22/ConsoleApp22/Lists.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp22
+{
+
+        internal class Lists<T>
+        {
+            public T[] lists;
+            public Lists()
+            {
+                lists = new T[0];
+            }
+            public void Add(T obj)
+            {
+                Array.Resize(ref lists, lists.Length + 1);
+
+
+                lists[lists.Length - 1] = obj;
+               
+
+
+            }
+
+        public T find(T OBJ)
+        {
+           
+            
+            foreach (var item in lists)
+            {
+               // if (OBJ == lists[item]) return item;
+
+            }
+            throw new Exception("tapilmadi");
+           
+        }
+
+    }
+}
+
---
+++ ConsoleApp22/ConsoleApp22/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ConsoleApp22
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Lists<String> lists = new Lists<String>();
+            string obj = new string("orxan");
+            string obj1 = new string("kenan");
+            lists.Add(obj1);
+            lists.Add(obj);
+            //lists.find(obj);
+            
+        }
+    }
+}
